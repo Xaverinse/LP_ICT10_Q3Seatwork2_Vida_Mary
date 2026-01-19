541216c4c24fe6e5d3668b38063e1a0865e43e3c
Chaid <vida.mkp@obmontessori.edu.ph>
2026-01-19 20:44:19 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+from pyscript import document
+
+def get_team(event):
+ 
+    medi = document.getElementById("medcert").value
+    regi = document.getElementById("regis").value
+
+    grade = document.getElementById("grade")
+    section = document.getElementById("section")
+    team_get = grade.value
+    team_get2 = section.value
+
+    # Determine Team Name
+    if document.querySelector('input[name="regis"]:checked').value == "No" and document.querySelector('input[name="medcert"]:checked').value == "No":
+        document.getElementById("team_name").innerText = ("Please continue your online registration and/or submit your medical certificate to join a team.")
+    elif document.getElementById("grade").value == "g7" and document.getElementById("section").value == "Sapphire":
+        document.getElementById("team_name").innerText = "You are a Blue Bear!"
+        document.getElementById("team_img").src = "blue.png"
+    elif document.getElementById("grade").value == "g7" and document.getElementById("section").value == "Emerald":
+        document.getElementById("team_name").innerText = "You are a Yellow Tiger!"
+        document.getElementById("team_img").src = "yellow.png"
+    elif document.getElementById("grade").value == "g7" and document.getElementById("section").value == "Ruby":
+        document.getElementById("team_name").innerText = "You are a Red Bulldog!"
+        document.getElementById("team_img").src = "red.png"
+    elif document.getElementById("grade").value == "g7" and document.getElementById("section").value == "Topaz":
+        document.getElementById("team_name").innerText = "You are a Green Hornet!"
+        document.getElementById("team_img").src = "green.png"
+    elif document.getElementById("grade").value == "g8" and document.getElementById("section").value == "Ruby":
+        document.getElementById("team_name").innerText = "You are a Red Bulldog!"
+        document.getElementById("team_img").src = "red.png"
+    elif document.getElementById("grade").value == "g8" and document.getElementById("section").value == "Sapphire":
+        document.getElementById("team_name").innerText = "You are a Yellow Tiger!"
+        document.getElementById("team_img").src = "yellow.png"
+    elif document.getElementById("grade").value == "g8" and document.getElementById("section").value == "Emerald":
+        document.getElementById("team_name").innerText = "You are a Green Hornet!"
+        document.getElementById("team_img").src = "green.png"
+    elif document.getElementById("grade").value == "g8" and document.getElementById("section").value == "Topaz":
+        document.getElementById("team_name").innerText = "You are a Blue Bear!"
+        document.getElementById("team_img").src = "blue.png"
+    elif document.getElementById("grade").value == "g9" and document.getElementById("section").value == "Emerald":
+        document.getElementById("team_name").innerText = "You are a Green Hornet!"
+        document.getElementById("team_img").src = "green.png"
+    elif document.getElementById("grade").value == "g9" and document.getElementById("section").value == "Topaz":
+        document.getElementById("team_name").innerText = "You are a Red Bulldog!"
+        document.getElementById("team_img").src = "red.png"
+    elif document.getElementById("grade").value == "g9" and document.getElementById("section").value == "Sapphire":
+        document.getElementById("team_name").innerText = "You are a Blue Bear!"
+        document.getElementById("team_img").src = "blue.png"
+    elif document.getElementById("grade").value == "g9" and document.getElementById("section").value == "Ruby":
+        document.getElementById("team_name").innerText = "You are a Yellow Tiger!"
+        document.getElementById("team_img").src = "yellow.png"
+    elif document.getElementById("grade").value == "g10" and document.getElementById("section").value == "Topaz":
+        document.getElementById("team_name").innerText = "You are a Yellow Tiger!"
+        document.getElementById("team_img").src = "yellow.png"
+    elif document.getElementById("grade").value == "g10" and document.getElementById("section").value == "Ruby":
+        document.getElementById("team_name").innerText = "You are a Blue Bear!"
+        document.getElementById("team_img").src = "blue.png"
+    elif document.getElementById("grade").value == "g10" and document.getElementById("section").value == "Sapphire":
+        document.getElementById("team_name").innerText = "You are a Green Hornet!!! You are also EXTREMELY GOATED (totally not baised)"
+        document.getElementById("team_img").src = "green.png"
+    elif document.getElementById("grade").value == "g10" and document.getElementById("section").value == "Emerald":
+        document.getElementById("team_name").innerText = "You are a Red Bulldog!"
+        document.getElementById("team_img").src = "red.png"
+    else:
+        document.getElementById("team_name").innerText = "You are a very super secret special case. Congradulations, but you have been chosen to be excempted from the intrams for being too unlucky or because I forgot how to fix my code"
+
